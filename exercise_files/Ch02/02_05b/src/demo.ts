a13122a2acf4ab7b6e04ee1dc234a53c8ef8fef8
Divineshia <divinejeba.sharon@gmail.com>
2023-09-12 16:06:16 -0500

    challenge 2 done

--- exercise_files/Ch02/02_05b/src/demo.ts
+++ exercise_files/Ch02/02_05b/src/demo.ts
@@ -1,8 +1,17 @@
 interface Contact {
     id: number;
     name: string;
+    //writing function inside the interface
+    //clone(name:string):Contact;
 }
 
-function clone(source) {
+function clone(source:Contact) :Contact{
+    //return source;
+    //return "Address";
     return Object.apply({}, source);
 }
+
+const a: Contact ={
+    id:123, name:"Andrew"
+};
+const b =clone(a)
--- exercise_files/Ch02/02_06b/src/demo.ts
+++ exercise_files/Ch02/02_06b/src/demo.ts
@@ -3,9 +3,20 @@ interface Contact {
     name: string;
 }
 
-function clone(source: Contact): Contact {
+interface UserContact {
+    id: number;
+    name: string;
+    username:string;
+}
+//using generics type T as metadata
+function clone<T>(source: T): T {
+    return Object.apply({}, source);
+}
+
+function clone1 <T1, T2 extends T1>(source:T1):T2{
     return Object.apply({}, source);
 }
 
 const a: Contact = { id: 123, name: "Homer Simpson" };
 const b = clone(a)
+const c=clone1<Contact, UserContact>(a)
